1669d396eba8f3f810f1c632e08ffd7006a18b5a
Babisama1 <kelvinlonelyboy@gmail.com>
2025-01-07 08:55:07 +0800

    Buat Tictactoe

---
+++ src/main/java/com/genz/App.java
@@ -0,0 +1,20 @@
+package com.genz;
+
+import com.genz.game.Game;
+import com.genz.player.Player;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Player player1 = new Player("Player 1", 'X');
+        Player player2 = new Player("Player 2", 'O');
+
+        Game game = new Game(player1, player2);
+        game.start();
+    }
+}
