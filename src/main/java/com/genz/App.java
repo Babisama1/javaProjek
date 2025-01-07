b98881163d16090a831e9fce22c2ea9741923938
Babisama1 <kelvinlonelyboy@gmail.com>
2025-01-07 21:55:14 +0800

    Memperbaiki 1.0

--- src/main/java/com/genz/App.java
+++ src/main/java/com/genz/App.java
@@ -1,16 +1,31 @@
 package com.genz;
 
-import com.genz.model.ModelExample;
-import com.genz.service.ServiceExample;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+import com.genz.game.Game;
+import com.genz.player.AIPlayer;
+import com.genz.player.Player;
+
 public class App {
     public static void main(String[] args) {
-        ModelExample model = new ModelExample("Hello from Model!");
-        ServiceExample service = new ServiceExample();
-        service.printMessage(model);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selamat datang di Tic Tac Toe!");
+        System.out.print("Apakah anda ingin melawan AI? (iya/tidak): ");
+        String choice = scanner.next();
+
+        Player player1 = new Player("Player 1", 'X');
+        Player player2;
+
+        if (choice.equalsIgnoreCase("iya")) {
+            player2 = new AIPlayer("AI", 'O');
+        } else {
+            player2 = new Player("Player 2", 'O');
+        }
+
+        Game game = new Game(player1, player2, choice.equalsIgnoreCase("yes"));
+        game.start();
+
+        scanner.close();
     }
 }
